b91556736ab9eafd57a4335ddd13485074736c17
Lyudmila Karpova <lkarpova@i-neti.ru>
2021-08-03 11:28:38 +0300

    auth-form

--- local/templates/v_new_template/components/bitrix/system.auth.changepasswd/popup/template.php
+++ local/templates/v_new_template/components/bitrix/system.auth.changepasswd/popup/template.php
@@ -1,6 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 use Bitrix\Main\Application;
+use Bitrix\Main\Localization\Loc;
 
 $request = Application::getInstance()->getContext()->getRequest();
 
@@ -8,10 +9,11 @@ if ($request->getPost("AJAX")) {
 	include(Application::getDocumentRoot() . $templateFolder . '/ajax.php');
 } else {
 	if (!$GLOBALS["USER"]->IsAuthorized()) {
-		include(Application::getDocumentRoot() . $templateFolder . '/registration.php');
+		LocalRedirect("/auth/");
 	} else {
-		LocalRedirect($arParams['PROFILE_URL']);
+		include(Application::getDocumentRoot() . $templateFolder . '/changepasswd.php');
 	}
+
 }
 
 ?>
--- local/templates/v_new_template/components/bitrix/system.auth.forgotpasswd/popup/template.php
+++ local/templates/v_new_template/components/bitrix/system.auth.forgotpasswd/popup/template.php
@@ -4,15 +4,13 @@ use Bitrix\Main\Application;
 
 $request = Application::getInstance()->getContext()->getRequest();
 
-
-if ($request->get("AJAX")) {
+if ($request->getPost("AJAX")) {
 	include(Application::getDocumentRoot() . $templateFolder . '/ajax.php');
 }else {
 	if (!$GLOBALS["USER"]->IsAuthorized()) {
-		include(Application::getDocumentRoot() . $templateFolder . '/auth.php');
+		include(Application::getDocumentRoot() . $templateFolder . '/forgot.php');
 	} else {
 		LocalRedirect($arParams['PROFILE_URL']);
 	}
 }
-
 ?>
