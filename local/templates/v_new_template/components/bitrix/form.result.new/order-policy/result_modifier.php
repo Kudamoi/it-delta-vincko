cf2322a011f831adca4132da9b00ca7255ed9525
Людмила Карпова <lyudmila.karpova.s@gmail.com>
2021-08-11 18:19:54 +0300

    t817 добавила классы и ид для верификации на фронте

--- local/templates/v_new_template/components/bitrix/form.result.new/order-policy/result_modifier.php
+++ local/templates/v_new_template/components/bitrix/form.result.new/order-policy/result_modifier.php
@@ -14,7 +14,7 @@ if (empty($arResult["POLICY_ID"]) && $arResult["isFormNote"] != "Y") {
 	LocalRedirect($arParams["BACK_LINK"]);
 }
 
-if($arResult["isFormNote"] == "Y"){
+if ($arResult["isFormNote"] == "Y") {
 	$arOrder = \CFormResult::GetDataByID(
 		$request->get("RESULT_ID"),
 		array("ORDER_ID"),
@@ -23,7 +23,25 @@ if($arResult["isFormNote"] == "Y"){
 	$arResult["ORDER_ID"] = $arOrder["ORDER_ID"][0]["USER_TEXT"];
 
 }
-
+// сформируем массив с особыми классами, необходимыми для верификации на фронте
+$domClass = [
+	"REGISTRATION_HOUSE"     => "house-field",
+	"REGISTRATION_APARTMENT" => "flat-field",
+	"REGISTRATION_INDEX"     => "index-field",
+	"ACTUAL_HOUSE"           => "house-field",
+	"ACTUAL_APARTMENT"       => "flat-field",
+	"ACTUAL_INDEX"           => "index-field",
+	"POLICY_HOUSE"           => "house-field",
+	"POLICY_APARTMENT"       => "flat-field",
+	"POLICY_INDEX"           => "index-field",
+];
+// сформируем массив с особыми id, необходимыми для верификации на фронте
+$domId = [
+	"PHONE"         => "phone-field",
+	"PASSPORT_CODE" => "code",
+	"INN"           => "inn",
+	"PASSPORT"      => "passport",
+];
 // отредактируем некоторые поля
 foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
 	if ($arQuestion['STRUCTURE'][0]['FIELD_TYPE'] != 'hidden') {
@@ -35,24 +53,23 @@ foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
 				$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] = "";
 				$name = "form_radio_" . $FIELD_SID;
 				$i = 0;
-
 				foreach ($arQuestion['STRUCTURE'] as $arAnsw) {
 					// если текущее поле "Адрес квартиры, для которой вы оформляете страховку", у ответа "Указать другой адрес" будет особый id и отображение, предусмотренное на верстке
-					if($FIELD_SID == 'POLICY_ADDRESS' && $arAnsw["VALUE"]== 3 ){
+					if ($FIELD_SID == 'POLICY_ADDRESS' && $arAnsw["VALUE"] == 3) {
 						$id = "other";
-					}else{
+					} else {
 						$id = $arAnsw["ID"];
 					}
-					if($FIELD_SID == 'GENDER' ){
-					$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] .= '<input type="radio"  id="' . $id . '" name="' . $name . '" value="' . $arAnsw["ID"] . '" ' . ($i == 0 ? " checked" : "") . '>
+					if ($FIELD_SID == 'GENDER') {
+						$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] .= '<input type="radio"  id="' . $id . '" name="' . $name . '" value="' . $arAnsw["ID"] . '" ' . ($i == 0 ? " checked" : "") . '>
 																	<label for="' . $id . '"></label>
 																	<label for="' . $id . '">' . $arAnsw["MESSAGE"] . '</label>';
-					}else{
-						$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] .= ($id != "other"?'  <div class="radio-wrapper">':'').'
+					} else {
+						$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] .= ($id != "other" ? '  <div class="radio-wrapper">' : '') . '
                                         <input type="radio"  id="' . $id . '" name="' . $name . '" value="' . $arAnsw["ID"] . '" ' . ($i == 0 ? " checked" : "") . '>
                                         <label for="' . $id . '"></label>
                                         <label for="' . $id . '">' . $arAnsw["MESSAGE"] . '</label>
-                                   '.($id != "other"?' </div>':'');
+                                   ' . ($id != "other" ? ' </div>' : '');
 					}
 					$i++;
 				}
@@ -61,21 +78,20 @@ foreach ($arResult["QUESTIONS"] as $FIELD_SID => $arQuestion) {
 				$name = "form_date_" . $idAnsw;
 				$value = ($arResult["arrVALUES"][$name] ? ' value="' . $arResult["arrVALUES"][$name] . '"' : '');
 
-			$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] = '
-				<input' . $value . ' type="text" class="date js-check-valid-field' . (!empty($arResult['FORM_ERRORS'][$FIELD_SID]) ? ' error' : '') . '" name="' . $name . '" placeholder="' . $arQuestion["CAPTION"] . '" onclick="BX.calendar({node: this, value: \''.date('d.m.Y').'\',field: this, bTime: false});">
+				$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] = '
+				<input' . $value . ' type="text" class="date js-check-valid-field' . (!empty($arResult['FORM_ERRORS'][$FIELD_SID]) ? ' error' : '') . '" name="' . $name . '" placeholder="' . $arQuestion["CAPTION"] . '" onclick="BX.calendar({node: this, value: \'' . date('d.m.Y') . '\',field: this, bTime: false});">
 				';
 				break;
 			case "email":
 				$name = "form_email_" . $idAnsw;
 				$value = ($arResult["arrVALUES"][$name] ? ' value="' . $arResult["arrVALUES"][$name] . '"' : '');
 
-				$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] = '<input' . $value. ' id="email-field"  class="js-check-valid-field' . (!empty($arResult['FORM_ERRORS'][$FIELD_SID]) ? ' error' : '') . '" data-field="' . $FIELD_SID . '" type="text" name="' . $name . '" placeholder="' . $arQuestion["CAPTION"] . '" value="">';
+				$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] = '<input' . $value . ' id="email-field"  class="js-check-valid-field' . (!empty($arResult['FORM_ERRORS'][$FIELD_SID]) ? ' error' : '') . '" data-field="' . $FIELD_SID . '" type="text" name="' . $name . '" inputmode="email"  placeholder="' . $arQuestion["CAPTION"] . '" value="">';
 				break;
 			case "text":
 				$name = "form_text_" . $idAnsw;
 				$value = ($arResult["arrVALUES"][$name] ? ' value="' . $arResult["arrVALUES"][$name] . '"' : '');
-
-				$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] = '<input' . $value . ' '.($FIELD_SID=='PHONE'?'id="phone-field"':'').'class="js-check-valid-field' . (!empty($arResult['FORM_ERRORS'][$FIELD_SID]) ? ' error' : '') . '" data-field="' . $FIELD_SID . '" type="text" name="' . $name . '" placeholder="' . $arQuestion["CAPTION"] . '" value="">';
+				$arResult["QUESTIONS"][$FIELD_SID]["HTML_CODE"] = '<input' . $value . ' ' . ($domId[$FIELD_SID] ? 'id="' . $domId[$FIELD_SID] . '"' : '') . 'class="js-check-valid-field' . ($domClass[$FIELD_SID] ?? "text-field") . (!empty($arResult['FORM_ERRORS'][$FIELD_SID]) ? ' error' : '') . '" data-field="' . $FIELD_SID . '" type="text" inputmode="text" name="' . $name . '" placeholder="' . $arQuestion["CAPTION"] . '" value="">';
 				break;
 			case "checkbox":
 				$name = "form_checkbox_" . $FIELD_SID . "[]";
