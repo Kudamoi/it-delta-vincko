283b362e055a40345be998c3bb3aa556b36e47c2
Александр Гавриленко <prog2@it-delta.ru>
2021-08-19 17:50:42 +0300

    complete bugfix t951

--- local/components/it-delta/iblock.content/templates/rating_forming/template.php
+++ local/components/it-delta/iblock.content/templates/rating_forming/template.php
@@ -38,21 +38,24 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
                     параметрам.
                 </div>
                 <div class="formation__left_btns">
-                    <?$i=0;foreach ($arResult as $item):$i++;?>
-                        <button class="formation__left_btn formation__left_btn-<?if($i==1): echo "one formation__left_btn-active"; elseif($i==2): echo 'two'; elseif($i==3): echo 'three'; else: echo 'other'; endif;?>">
-                            <div class="formation__left_btn-border">
-                                <svg height="36" width="36">
-                                    <circle cx="17" cy="19" r="16"/>
-                                </svg>
-                            </div>
-                            <div class="formation__left_btn-icon">
-                                <img src="<?=$item['PREVIEW_PICTURE']?>" alt="">
-                            </div>
-                            <div class="formation__left_btn-text">
-                                <?=$item['NAME']?>
-                            </div>
-                        </button>
-                    <?endforeach; ?>
+                    <? $i = 1;
+                    foreach ($arResult as $item): ?>
+                        <? if ($item['ID'] != 49): ?>
+                            <button data-slide="<?=$i?>" class="formation__left_btn<?= $i++ == 1 ? " formation__left_btn-active" : ''; ?>">
+                                <div class="formation__left_btn-border">
+                                    <svg height="36" width="36">
+                                        <circle cx="17" cy="19" r="16"/>
+                                    </svg>
+                                </div>
+                                <div class="formation__left_btn-icon">
+                                    <img src="<?= $item['PREVIEW_PICTURE'] ?>" alt="">
+                                </div>
+                                <div class="formation__left_btn-text">
+                                    <?= $item['NAME'] ?>
+                                </div>
+                            </button>
+                        <? endif; ?>
+                    <? endforeach; ?>
                 </div>
                 <div class="formation__left_bottomText">
                     Из них формируется средняя оценка и определяет место компании в Репутационном рейтинге. Комбинация
@@ -60,7 +63,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
                     дает исчерпывающее представление об охранной организации.
                 </div>
                 <div class="formation__left_bottomBtn">
-                    <a href="/reviews/">
+                    <a href="/review-add/">
                         Оставить отзыв
                     </a>
                     <a href="#rating-center">
@@ -69,51 +72,50 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
                 </div>
             </div>
             <div class="formation__right">
-                <?$i=0;foreach ($arResult as $item):$i++;?>
-                <div class="formation__right_slide slide-<?if($i==1): echo "one formation__right_slide-active"; elseif($i==2): echo 'two'; elseif($i==3): echo 'three'; else: echo 'other'; endif;?>">
-                    <div class="formation__right_arrows">
-                        <div class="formation__right_arrow hide">
-                            <img src="/upload/rating/formation__right_arrow1.svg" alt="img">
+                <? $j = 1;foreach ($arResult as $item):?>
+                    <div data-slide="<?=$j?>" class="formation__right_slide<?= $j++ == 1 ? " formation__right_slide-active" : '' ?>">
+                        <div class="formation__right_arrows">
+                            <div class="formation__right_arrow left hide">
+                                <img src="/upload/rating/formation__right_arrow1.svg" alt="img">
+                            </div>
+                            <div class="formation__right_arrow right">
+                                <img src="/upload/rating/formation__right_arrow2.svg" alt="img">
+                            </div>
                         </div>
-                        <div class="formation__right_arrow slide-one-btnTwo">
-                            <img src="/upload/rating/formation__right_arrow2.svg" alt="img">
-                        </div>
-                    </div>
-                    <div class="formation__right_info">
-                        <div class="formation__right_info-top">
-                            <div class="formation__right_info-iconBig">
-                                <div class="formation__right_info-border">
-                                    <svg height="36" width="36">
-                                        <circle cx="17" cy="19" r="16"/>
-                                    </svg>
+                        <div class="formation__right_info">
+                            <div class="formation__right_info-top">
+                                <div class="formation__right_info-iconBig">
+                                    <div class="formation__right_info-border">
+                                        <svg height="36" width="36">
+                                            <circle cx="17" cy="19" r="16"/>
+                                        </svg>
+                                    </div>
+                                    <div class="formation__right_info-icon">
+                                        <img src="<?= $item['PREVIEW_PICTURE'] ?>" alt="">
+                                    </div>
                                 </div>
-                                <div class="formation__right_info-icon">
-                                    <img src="<?=$item['PREVIEW_PICTURE']?>" alt="">
+                                <div class="formation__right_info-text">
+                                    <?= $item['NAME'] ?>
                                 </div>
                             </div>
-                            <div class="formation__right_info-text">
-                                <?=$item['NAME']?>
+                            <div class="formation__right_info-textBottom">
+                                <?= $item['DESCRIPTION'] ?>
                             </div>
                         </div>
-                        <div class="formation__right_info-textBottom">
-                            <?=$item['DESCRIPTION']?>
+                        <div class="formation__right_questions">
+                            <? $z = 1; foreach ($item['ITEMS'] as $sItem): ?>
+                                <div class="formation__right_questionItem toggle__item<?= $z == 1 ? ' toggle__item_active' : '' ?>">
+                                    <div class="formation__right_questionItem-title toggle__item_title<?= $z == 1 ? ' toggle__item_title-active' : '' ?>">
+                                        <?= $sItem['NAME'] ?>
+                                    </div>
+                                    <div class="formation__right_questionItem-descr toggle__item_descr" <?= $z++ == 1? 'style="display:block;"' : '' ?>>
+                                        <?= $sItem['PREVIEW_TEXT'] ?>
+                                    </div>
+                                </div>
+                                <? endforeach; ?>
                         </div>
                     </div>
-                    <div class="formation__right_questions">
-                        <?$first = true;foreach ($item['ITEMS'] as $sItem):?>
-                        <div class="formation__right_questionItem toggle__item<?=$first?' toggle__item_active':''?>">
-                            <div class="formation__right_questionItem-title toggle__item_title<?=$first?' toggle__item_title-active':''?>">
-                                <?=$sItem['NAME']?>
-                            </div>
-                            <div class="formation__right_questionItem-descr toggle__item_descr" <?=$first?'style="display:block;"':''?>">
-                                <?=$sItem['PREVIEW_TEXT']?>
-                            </div>
-                        </div>
-                        <?$first = false;endforeach;?>
-
-                    </div>
-                </div>
-                <?endforeach;?>
+                <? endforeach; ?>
             </div>
         </div>
     </div>
