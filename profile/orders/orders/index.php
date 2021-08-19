f4402bd895584216e5db7fab55eac45b0cfa129c
Людмила Карпова <lyudmila.karpova.s@gmail.com>
2021-08-19 17:41:12 +0300

    t863 личный кабинет

--- profile/orders/orders/index.php
+++
@@ -1,8 +0,0 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("Заказы и заявки");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:sale.order.payment",
-	"",
-Array()
-);?><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
