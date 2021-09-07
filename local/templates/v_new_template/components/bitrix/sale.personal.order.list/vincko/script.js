526d6e72189c42a33f25ee6114990b507b4a03d2
Людмила Карпова <lyudmila.karpova.s@gmail.com>
2021-09-07 15:07:21 +0300

    правки по задаче

--- local/templates/v_new_template/components/bitrix/sale.personal.order.list/vincko/script.js
+++ local/templates/v_new_template/components/bitrix/sale.personal.order.list/vincko/script.js
@@ -7,4 +7,40 @@ $(document).ready(function () {
 			$(".profile__c-main--personal-requests").addClass("show");
 		}
 	});
+
+	$(".js-paid").click(function () {
+		var $parent = $(this).parent();
+		url = $parent.attr("data-url"),
+			paySystemId = $(this).attr("data-pay"),
+			accountNumber = $parent.attr("data-accountNumber"),
+			paymentNumber = $parent.attr("data-paymentNumber");
+		$parent
+			.parents(".profile__c-main-order-bottom")
+			.find(".profile__c-main-order-bottom-wrapper")
+			.append("<div class='profile__c-main-order-bottom-answ js-answer'>"
+				+ "Идет переадресация на страницу оплаты"
+				+ "</div>"
+			);
+		$.ajax({
+			url: url,
+			method: 'POST',
+			data:
+				{
+					sessid: BX.bitrix_sessid(),
+					paySystemId: paySystemId,
+					accountNumber: accountNumber,
+					paymentNumber: paymentNumber
+				},
+			dataType: 'html',
+			success: function (res) {
+				$parent.append(res);
+				console.log(res);
+			},
+			error: function (error) {
+				console.log(error);
+			}
+		});
+		return false;
+	});
+
 });
